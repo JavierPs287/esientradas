c1510f20561f9acfd2c1f7a489ac600aab2cbdf1
JavierPs287 <your-email@example.com>
2026-02-17 12:16:55 +0100

    Clase 17/02

---
+++ src/main/java/edu/esi/ds/esientradas/services/BusquedaService.java
@@ -0,0 +1,16 @@
+package edu.esi.ds.esientradas.services;
+
+import java.util.List;
+
+import edu.esi.ds.esientradas.model.Escenario;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BusquedaService {
+
+
+    public List<Escenario> getEscenarios() {
+        return null;
+    }
+}
