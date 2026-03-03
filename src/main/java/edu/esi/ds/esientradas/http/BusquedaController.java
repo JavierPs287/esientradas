a7e312f3b31de3a71420d5d703a0541a5a2edade
JavierPs287 <your-email@example.com>
2026-03-03 12:40:15 +0100

    .

--- src/main/java/edu/esi/ds/esientradas/http/BusquedaController.java
+++ src/main/java/edu/esi/ds/esientradas/http/BusquedaController.java
@@ -3,6 +3,7 @@ package edu.esi.ds.esientradas.http;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import edu.esi.ds.esientradas.services.BusquedaService;
 
 @RestController
 @RequestMapping("/busqueda")
+@CrossOrigin(origins = "*")
 public class BusquedaController {
 
     @Autowired
