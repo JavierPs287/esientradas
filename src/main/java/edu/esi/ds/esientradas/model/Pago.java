1fc9dde61b4773722101709c9ef256f50d362821
JavierPs287 <your-email@example.com>
2026-03-09 14:11:45 +0100

    Fix funcional

--- src/main/java/edu/esi/ds/esientradas/model/Pago.java
+++ src/main/java/edu/esi/ds/esientradas/model/Pago.java
@@ -1,8 +1,21 @@
 package edu.esi.ds.esientradas.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Pago {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
