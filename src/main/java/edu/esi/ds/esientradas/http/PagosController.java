8de775cb951b43c1a14d83ab8dc70e2129be094f
Berto845 <alberto.rosa2@alu.uclm.es>
2026-03-09 14:20:43 +0100

    Empezar prepago

--- src/main/java/edu/esi/ds/esientradas/http/PagosController.java
+++ src/main/java/edu/esi/ds/esientradas/http/PagosController.java
@@ -1,8 +1,36 @@
 package edu.esi.ds.esientradas.http;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.esi.ds.esientradas.services.PagosService;
+
 @RestController
+@RequestMapping("/pagar")
+@CrossOrigin(origins = "http://localhost:4200",
+allowCredentials = "true"
+)
 public class PagosController {
+
+    @Autowired
+    PagosService pagosService;
+
+    @PostMapping("/prepararPago")
+    public void prepararPago(@RequestBody Map<String, Object> infoPago) {
+        Long centimos = ((Number) infoPago.get("centimos")).longValue();
+        this.pagosService.prepararPago(infoPago);
+    }
+
+    @PostMapping("/confirmar")
+    public String confirmarPago() {
+        return this.pagosService.confirmarPago();
+    }
 
 }
--- src/main/java/edu/esi/ds/esientradas/services/PagosService.java
+++ src/main/java/edu/esi/ds/esientradas/services/PagosService.java
@@ -1,8 +1,26 @@
 package edu.esi.ds.esientradas.services;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import edu.esi.ds.esientradas.dao.ConfiguracionDAO;
 
 @Service
 public class PagosService {
+
+    @Autowired
+    ConfiguracionDAO configuracionDAO;
+
+    private static final String secretKey = "key";
+
+    public String prepararPago(Map<String,Object> infoPago) {
+        return null;
+    }
+
+    public String confirmarPago() {
+        return null;
+    }
 
 }
